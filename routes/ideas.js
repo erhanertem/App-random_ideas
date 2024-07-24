3db72c4f3656e80f0f769109e92ba03440262d6d
Erhan Ertem <eertem@gmail.com>
2024-07-24 15:07:54 +0300

    Update the entire API to use mongoDB

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,68 +1,84 @@
 const express = require('express');
 
-// TEMP DATA
-const ideas = [
-	{ id: 1, text: 'hello world', tag: 'software', username: 'Brunnel', date: '2022-01-01' },
-	{ id: 2, text: 'saassaahello world', tag: 'toftware', username: 'Zrunnel', date: '2024-01-01' },
-];
+const Idea = require('../models/Idea');
 
 // CREATE EXPRESS ROUTER FOR IDEAS
 const router = express.Router();
 
 // GET ALL IDEAS
-router.get('/', (req, res) => {
-	res.json({ success: true, data: ideas });
+router.get('/', async (req, res) => {
+	try {
+		const allIdeas = await Idea.find();
+		res.json({ success: true, data: allIdeas });
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).json({ success: false, error: 'Server Error' });
+	}
 });
 // GET A SINGLE IDEA
-router.get('/:id', (req, res) => {
-	const idea = ideas.find((idea) => idea.id === Number(req.params.id));
-
-	if (!idea) {
-		res.status(404).json({ success: false, error: 'Resource not found' });
+router.get('/:id', async (req, res) => {
+	try {
+		const idea = await Idea.findById(req.params.id);
+		// GUARD CLAUSE
+		if (!idea) {
+			return res.status(404).json({ success: false, error: 'Resource not found' });
+		}
+		res.json({ success: true, data: idea });
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).json({ success: false, error: 'Server Error' });
 	}
-
-	res.json({ success: true, data: idea });
 });
 
-// CREATE A NEW IDEA
-router.post('/', (req, res) => {
-	const idea = {
-		id: ideas.length + 1,
+// ADD AN IDEA
+router.post('/', async (req, res) => {
+	const idea = new Idea({
 		text: req.body.text,
 		tag: req.body.tag,
 		username: req.body.username,
-		date: new Date().toISOString().split('T')[0],
-	};
+	});
 
-	ideas.push(idea);
-
-	res.send(req.body.text);
+	try {
+		const savedIdea = await idea.save();
+		res.json({ success: true, data: savedIdea });
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).json({ success: false, error: 'Server Error' });
+	}
 });
 
-// UPDATE IDEA
-router.put('/:id', (req, res) => {
-	const idea = ideas.find((idea) => idea.id === Number(req.params.id));
-	// GUARD CLAUSE
-	if (!idea) {
-		return res.status(404).json({ success: false, error: 'Resource not found' });
+// UPDATE AN IDEA
+router.put('/:id', async (req, res) => {
+	try {
+		const updatedIdea = await Idea.findByIdAndUpdate(
+			req.params.id,
+			{ $set: { text: req.body.text, tag: req.body.tag } },
+			{ new: true }
+		);
+		// GUARD CLAUSE
+		if (!updatedIdea) {
+			return res.status(404).json({ success: false, error: 'Resource not found' });
+		}
+		res.json({ success: true, data: updatedIdea });
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).json({ success: false, error: 'Server Error' });
 	}
-
-	idea.text = req.body.text || idea.text;
-	idea.tag = req.body.tag || idea.tag;
-
-	res.json({ success: true, data: idea });
 });
 
 // DELETE IDEA
-router.delete('/:id', (req, res) => {
-	const ideaIndex = ideas.findIndex((idea) => idea.id === Number(req.params.id));
-	// GUARD CLAUSE
-	if (ideaIndex === -1) {
-		return res.status(404).json({ success: false, error: 'Resource not found' });
+router.delete('/:id', async (req, res) => {
+	try {
+		// await Idea.findOneAndDelete({ _id: req.params.id }); //Below method is d shorthand method
+		const deletedIdea = await Idea.findByIdAndDelete(req.params.id);
+		// GUARD CLAUSE
+		if (!deletedIdea) {
+			return res.status(404).json({ success: false, error: 'Resource not found' });
+		}
+		res.json({ success: true, data: {} });
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).json({ success: false, error: 'Server Error' });
 	}
-
-	// DELETE IDEA FROM ARRAY
-	ideas.splice(ideaIndex, 1);
-	res.json({ success: true, message: 'Idea deleted successfully' });
 });
 module.exports = router;
