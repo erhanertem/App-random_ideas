a5a9942377f2822643cc89ccf759826a042117d9
Erhan Ertem <eertem@gmail.com>
2024-07-24 17:00:17 +0300

    Implement modal open/close functionality

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,21 @@
+// import axios from 'axios';
+
 import './css/style.css';
+
+const modal = document.querySelector('#modal');
+const modalBtn = document.querySelector('#modal-btn');
+
+function openModal() {
+	modal.style.display = 'block';
+}
+function closeModal() {
+	modal.style.display = 'none';
+}
+function outsideClick(event) {
+	// console.log(event.target);
+	if (event.target === modal) {
+		closeModal();
+	}
+}
+modalBtn.addEventListener('click', openModal);
+window.addEventListener('click', outsideClick);
